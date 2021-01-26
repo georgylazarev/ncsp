106e5204f894d33b4d9c513ad2c386c541a69f08
Георгий Лазарев <geolaz@mail.com>
2021-01-26 10:41:29 +0300

    two parts variant

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 'use strict';
 
 (function () {
+  document.querySelector('.logo__image--left').style.display = 'none';
+  document.querySelector('.logo__image--right').style.display = 'none';
   document.querySelector('.logo__title').style.display = 'none';
+  document.querySelector('.logo__image--left').style.display = 'block';
+  setTimeout(() => {
+    document.querySelector('.logo__image--right').style.display = 'block'
+  }, 1100);
   setTimeout(() => {
     document.querySelector('.logo__title').style.display = 'inline'
-  }, 1200);
+  }, 2400);
 })();
