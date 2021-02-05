d83c23b5bebba240c6bf953dc3d4d47cb14efd39
Георгий Лазарев <geolaz@mail.com>
2021-02-05 12:08:30 +0300

    some changes

--- js/script.js
+++ js/script.js
@@ -6,6 +6,9 @@
     document.querySelector('.logo__title').style.display = 'inline'
   }, 1700);
   setTimeout(() => {
-    document.querySelector('.contacts').style.display = 'inline'
-  }, 2400);
+    document.querySelector('.logo__email').style.display = 'block'
+  }, 2600);
+  setTimeout(() => {
+    document.querySelector('.logo__address').style.display = 'block'
+  }, 2600);
 })();
